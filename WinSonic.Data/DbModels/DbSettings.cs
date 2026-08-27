86e9a3a835172f51bf1eeb801d2e78e5b99d7bf8
Lee Butler <github@leerb.co.uk>
2026-08-27 13:20:11 +0100

    Settings in DB?

---
+++ WinSonic.Data/DbModels/DbSettings.cs
@@ -0,0 +1,31 @@
+using WinSonic.Data.Enums;
+
+namespace WinSonic.Data.DbModels;
+
+public class DbSettings
+{
+    public int Id { get; set; }
+    
+    // General settings
+    public bool CheckForUpdates { get; set; }
+    public string LanguageIetf { get; set; }
+    public string? ThemeKey { get; set; }
+    public bool SyncLyrics { get; set; }
+    
+    // Playback settings
+    public string? OutputDevice { get; set; }
+    public ReplayGainMode ReplayGainMode { get; set; }
+    public ReplayGainClippingPrevention ClippingPrevention { get; set; }
+    public double? Preamp { get; set; }
+    
+    // Server settings
+    public string ServerAddress { get; set; }
+    public string Username { get; set; }
+    public string PasswordCredentialKey { get; set; }
+    public bool IgnoreSslErrors { get; set; }
+    public bool ScrobbleToServer { get; set; }
+    public double? ScrobbleMinimumPercentage { get; set; }
+    public double? ScrobbleMinimumSeconds { get; set; }
+    public bool SyncPlayQueue { get; set; }
+    
+}
---
+++ WinSonic.Data/Enums/SettingsEnums.cs
@@ -0,0 +1,17 @@
+namespace WinSonic.Data.Enums;
+
+public enum ReplayGainMode
+{
+    None, 
+    Auto, 
+    Album,
+    Track
+}
+
+public enum ReplayGainClippingPrevention
+{
+    Off,
+    ReduceGain
+}
+
+
